eb5352179bcda39002c861a3589c3cc3de66c938
FGHII <eric.j.klosterman@gmail.com>
2021-04-21 17:19:06 -0500

    updated HTML to show score, round winner and game winner

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function computerPlay() {
   const options = ["rock", "paper", "scissors"];
   let computerResIndex = Math.floor(Math.random() * 3);
@@ -7,33 +5,23 @@ function computerPlay() {
   return computerResponse;
 };
 
-// function playerPlay() {
-//   // let playerResponse = prompt("Enter Rock, Paper, or Scissors:");
-//   playerResponse = playerResponse.toLowerCase();
-//   return playerResponse;
-// };
-
 function playRound(gameScore, playerResponse) {
   let computerSelection = computerPlay();
   let playerSelection = playerResponse;
-  console.log(computerSelection);
-  console.log(playerSelection);
   if ((computerSelection === "paper" && playerSelection === "rock") ||
     (computerSelection === "scissors" && playerSelection === "paper") ||
     (computerSelection === "rock" && playerSelection === "scissors")) {
-    console.log("Computer wins, " + computerSelection + " beats " + playerSelection);
+    document.querySelector(".round-winner").innerText = "Computer wins, " + computerSelection + " beats " + playerSelection;
     gameScore.computerWinCount++;
-    console.log("Computer: " + gameScore.computerWinCount + " Player: " + gameScore.playerWinCount);
   } else if ((computerSelection === "scissors" && playerSelection === "rock") ||
     (computerSelection === "rock" && playerSelection === "paper") ||
     (computerSelection === "paper" && playerSelection === "scissors")) {
-    console.log("Player wins, " + playerSelection + " beats " + computerSelection);
+    document.querySelector(".round-winner").innerText = "Player wins, " + playerSelection + " beats " + computerSelection;
     gameScore.playerWinCount++;
-    console.log("Computer: " + gameScore.computerWinCount + " Player: " + gameScore.playerWinCount);
   } else {
-    console.log("It's a draw");
-    console.log("Computer: " + gameScore.computerWinCount + " Player: " + gameScore.playerWinCount);
+    document.querySelector(".round-winner").innerText = "It's a draw";
   }
+  document.querySelector(".results p").innerText = "Computer: " + gameScore.computerWinCount + " Player: " + gameScore.playerWinCount;
   return gameScore;
 };
 
@@ -42,25 +30,28 @@ function game() {
     playerWinCount: 0,
     computerWinCount: 0
   };
+  let winFlag = false;
   const buttons = document.querySelectorAll("button");
   buttons.forEach(function(button) {
-    button.addEventListener("click",function() {
+    button.addEventListener("click", function() {
       const playerResponse = event.target.innerText.toLowerCase();
       gameScore = playRound(gameScore, playerResponse);
-      console.log(gameScore);
+      if (gameScore.playerWinCount === 5) {
+        document.querySelector(".game-winner").innerText = "The winner of 5 rounds is player!";
+      } else if (gameScore.computerWinCount === 5) {
+        document.querySelector(".game-winner").innerText = "The winner of 5 rounds is computer!";
+      }
     });
   });
-}
-  // let winFlag = false;
   // while (winFlag == false) {
-  //   })
-
-    // if (gameScore.playerWinCount === 5) {
-    //   winFlag = true;
-    //   console.log("The winner of 5 rounds is player!");
-    // } else if (gameScore.computerWinCount === 5) {
-    //   winFlag = true;
-    //   console.log("The winner of 5 rounds is computer!");
-    // }
+  //   if (gameScore.playerWinCount === 5) {
+  //     winFlag = true;
+  //     console.log("The winner of 5 rounds is player!");
+  //   } else if (gameScore.computerWinCount === 5) {
+  //     winFlag = true;
+  //     console.log("The winner of 5 rounds is computer!");
+  //   }
+  // }
+}
 
 game();
